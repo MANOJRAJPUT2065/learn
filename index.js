d77fef34ab8d0971fb24aa2649c1dacc0ddfc042
Manoj Rajput <smanojsingh073@gmail.com>
2023-11-14 23:20:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+ 
+   res.send("manojdotcom")
+  
+  })
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
